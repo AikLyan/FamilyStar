a6a1464a60b6a2963b42dcc221f9036682b9f8b0
AikLyan <kotayza2001@gmail.com>
2026-06-14 17:20:33 +0630

    initial upload of Family Star project

---
+++ MainActivity.kt
@@ -0,0 +1,20 @@
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // 👉 YOUR WEBSITE LINK HERE
+        webView.loadUrl("https://your-github-pages-link.com")
+    }
+}
