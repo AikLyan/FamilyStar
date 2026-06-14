d007d4e37dafb86ed64f1bddfb798d4b59e5772c
AikLyan <kotayza2001@gmail.com>
2026-06-14 15:18:38 +0630

    initial upload of Family Star project

--- checkout.js
+++ checkout.js
@@ -1,46 +1,60 @@
 import { db } from "./firebase.js";
-import { doc, getDoc, addDoc, collection, updateDoc } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+import {
+  doc,
+  getDoc,
+  addDoc,
+  collection,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
 const productId = localStorage.getItem("productId");
 const user = localStorage.getItem("user");
 
-if (!productId) location.href = "shop.html";
+let productData;
 
-let product;
-
+/* LOAD PRODUCT */
 async function load() {
 
-  const snap = await getDoc(doc(db, "products", productId));
-  product = snap.data();
+  const snap = await getDoc(doc(db, "product", productId));
+
+  productData = snap.data();
 
   document.getElementById("checkout").innerHTML = `
-    <h3>${product.name}</h3>
-    <p>${product.price}</p>
+    <h3>${productData.name}</h3>
+    <p>${productData.price} MMK</p>
     <button onclick="order()">Confirm</button>
   `;
+
 }
 
+/* ORDER */
 window.order = async function () {
 
   const userRef = doc(db, "users", user);
-  const snap = await getDoc(userRef);
+  const userSnap = await getDoc(userRef);
 
-  const balance = snap.data().balance || 0;
+  const balance = userSnap.data().balance || 0;
 
-  if (balance < product.price) return alert("Not enough balance");
+  if (balance < productData.price) {
+    alert("Not enough balance");
+    return;
+  }
 
   await updateDoc(userRef, {
-    balance: balance - product.price
+    balance: balance - productData.price
   });
 
   await addDoc(collection(db, "orders"), {
     username: user,
-    productName: product.name,
-    price: product.price,
+    productId,
+    productName: productData.name,
+    price: productData.price,
     status: "pending",
     createdAt: Date.now()
   });
 
+  alert("Order Success");
   location.href = "myorders.html";
 };
 
