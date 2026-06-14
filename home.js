e2aaf81ab8e72a3ca920268e437a2147e85d6316
AikLyan <kotayza2001@gmail.com>
2026-06-14 15:03:54 +0630

    initial upload of Family Star project

--- home.js
+++ home.js
@@ -1,21 +1,54 @@
 import { db } from "./firebase.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
 const user = localStorage.getItem("user");
 
-if (!user) location.href = "index.html";
-
-async function load() {
-
-  const snap = await getDoc(doc(db, "users", user));
-
-  if (!snap.exists()) return;
-
-  const d = snap.data();
-
-  document.getElementById("username").innerText = user;
-  document.getElementById("balance").innerText = (d.balance || 0) + " MMK";
-  document.getElementById("point").innerText = (d.point || 0) + " PT";
+/* LOGIN CHECK */
+if (!user) {
+  window.location.href = "index.html";
+  throw new Error("User not logged in");
 }
 
-load();
+/* LOAD USER INFO */
+async function loadUser() {
+
+  try {
+
+    const ref = doc(db, "users", user);
+
+    const snap = await getDoc(ref);
+
+    if (!snap.exists()) {
+
+      document.getElementById("username").innerText = user;
+      document.getElementById("balance").innerText = "0 MMK";
+      document.getElementById("point").innerText = "0 PT";
+
+      return;
+    }
+
+    const data = snap.data();
+
+    document.getElementById("username").innerText = user;
+
+    document.getElementById("balance").innerText =
+      (data.balance || 0) + " MMK";
+
+    document.getElementById("point").innerText =
+      (data.point || 0) + " PT";
+
+  } catch (err) {
+
+    console.error(err);
+
+    alert("Failed to load user data");
+
+  }
+
+}
+
+loadUser();
