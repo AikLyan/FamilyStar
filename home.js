922c0b962ebf82a84283772e4965211bc46ab5e1
AikLyan <kotayza2001@gmail.com>
2026-06-14 17:09:59 +0630

    initial upload of Family Star project

--- home.js
+++ home.js
@@ -1,19 +1,38 @@
-import { auth, db } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+import { db } from "./firebase.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
-onAuthStateChanged(auth, async (user) => {
+window.login = async function () {
 
-  if (!user) {
-    location.href = "index.html";
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (!username || !password) {
+    alert("Fill all fields");
     return;
   }
 
-  const snap = await getDoc(doc(db, "users", user.uid));
+  const ref = doc(db, "users", username);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    alert("User not found");
+    return;
+  }
 
   const data = snap.data();
 
-  document.getElementById("username").innerText = data.email;
-  document.getElementById("balance").innerText = (data.balance || 0) + " MMK";
-  document.getElementById("point").innerText = (data.point || 0) + " PT";
-});
+  if (data.password !== password) {
+    alert("Wrong password");
+    return;
+  }
+
+  localStorage.setItem("user", username);
+  localStorage.setItem("role", data.role);
+
+  alert("Login Success");
+
+  location.href =
+    data.role === "admin"
+      ? "admin-home.html"
+      : "home.html";
+};
