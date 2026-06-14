4aee74035d084846e18fed784c99bc0222aa26d9
AikLyan <kotayza2001@gmail.com>
2026-06-14 11:48:44 +0630

    initial upload of Family Star project

--- auth.js
+++ auth.js
@@ -1,22 +1,26 @@
 window.login = function () {
 
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
+  const u = document.getElementById("username").value.trim();
+  const p = document.getElementById("password").value.trim();
 
-  if (!username || !password) {
+  if (!u || !p) {
     alert("Fill all fields");
     return;
   }
 
-  if (username === "admin" && password === "1234") {
-
-    localStorage.setItem("username", username);
-
-    alert("Login success");
-
-    window.location.href = "home.html";
-
-  } else {
-    alert("Wrong username or password");
+  if (u === "admin" && p === "1234") {
+    localStorage.setItem("username", u);
+    localStorage.setItem("role", "admin");
+    window.location.href = "admin.html";
+    return;
   }
+
+  if (u === "user" && p === "1234") {
+    localStorage.setItem("username", u);
+    localStorage.setItem("role", "user");
+    window.location.href = "home.html";
+    return;
+  }
+
+  alert("Wrong login");
 };
